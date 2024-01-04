8766a6c5c437eac042383261cba58474072078f2
shetty-nithin <shettynithin744@gmail.com>
2024-01-05 00:48:21 +0530

    updated the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './about.scss';
-// import saffronBlueImage from '../../photos/colorfull.png'
+import saffronBlueImage from '../../photos/nandihill.jpg'
 
 const About = () => {
     return (
         <div id='aboutSection' className="aboutSection container">
             <div className="aboutePhoto col-lg-4 col-md-5 col-sm-7">
                 <div className='photo'>
-                    {/* <img src={saffronBlueImage} alt="nithin's"/> */}
+                    <img src={saffronBlueImage} alt="nithin's"/>
                 </div>
             </div>
             <div className="abouteText col-lg-8 col-md-7">
@@ -17,7 +17,11 @@ const About = () => {
                 </div>
                 <div className="text">
                     <p>
-                        In the sitcom world of software development, I am the Chandler, bringing witty intelligence to coding. As a CBTC Verification engineer at Alstom, I take on the role of Ross, ensuring <span className='train'>trains</span> run smoothly. My passion for coding is Joey-level, and with every line of code, I create a Phoebe-like symphony. Like Monica's precision, I craft elegant solutions while staying trendy like Rachel. My enthusiasm, akin to Joey's laughter, is contagious, inspiring the team around me. In this "Friends" of coding, I am destined to be the Ross and Rachel, making an extraordinary impact!
+                        My journey spans roles in system verification and validation at Alstom and Hitachi Rail STS.
+                        While my current involvement in various metro projects focuses on 'signalling' system verification, my true passion lies in being a dedicated and enthusiastic software developer. Transitioning into full-fledged software development, I'm dedicated to creating user-friendly web applications using .NET, ReactJs, and NodeJs, C++ etc. Beyond my professional journey, personal projects like Roomz.com and Airline Reservation reflect my authentic love for coding.
+                        <br/>
+                        <br/>
+                        Currently seeking opportunities as a software developer/engineer, I'm eager to collaborate, learn, and make a meaningful impact with an insatiable thirst for knowledge.
                     </p>
                 </div>
                 <div className="education">
